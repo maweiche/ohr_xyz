a2f8af1c7ccc31d9fb6f2caa7dc1538475d83f64
Noam <noamru22@gmail.com>
2023-11-27 12:47:39 +0100

    test: new env vars added for kv-stoer

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -5,15 +5,30 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const { name, email, telegram } = request.body;
-  const timestamp = Date.now();
-  const userId = crypto.randomUUID();
-  const userData = { name, email, telegram, timestamp };
+  if (request.method === "GET") {
+    try {
+      const userIds = await kv.keys("*");
+      const usersPromises = userIds.map(async (userId) => {
+        return await kv.hgetall(userId);
+      });
+      const users = await Promise.all(usersPromises);
 
-  try {
-    await kv.hmset(userId, userData);
-    return response.status(200).send("Email sent and contact saved");
-  } catch (err) {
-    return response.status(500).send("Internal error");
+      return response.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      return response.status(500).send("Internal error");
+    }
+  } else if (request.method === "POST") {
+    const { name, email, telegram } = request.body;
+    const timestamp = Date.now();
+    const userId = crypto.randomUUID();
+    const userData = { name, email, telegram, timestamp };
+
+    try {
+      await kv.hmset(userId, userData);
+      return response.status(200).send("Email sent and contact saved");
+    } catch (err) {
+      return response.status(500).send("Internal error");
+    }
   }
 }
