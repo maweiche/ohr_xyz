2b17dc58d1cc38c03050d5e48ba4fcb8448775e5
Noam <noamru22@gmail.com>
2023-12-05 14:34:14 +0100

    test: x link preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,17 +23,22 @@ export default function Home() {
         <title>øhr</title>
         <meta name="description" content="We believe in the power of sound!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:url" content="https://ohr-app.xyz" />
-        <meta property="og:type" content="website" />
-        <meta property="fb:app_id" content="your fb app id" />
-        <meta property="og:title" content="Hear my øhr" />
         <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@nytimesbits" />
+        <meta name="twitter:creator" content="@nickbilton" />
+        <meta
+          property="og:url"
+          content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+        />
+        <meta property="og:title" content="A Twitter for My Sister" />
         <meta
           property="og:description"
-          content="Hear this out! I minted an øhr  "
+          content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
         />
-        <meta property="og:image" content={"url of image"} />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+        />
       </Head>
       <LayoutComponent
         showNavBar={true}
