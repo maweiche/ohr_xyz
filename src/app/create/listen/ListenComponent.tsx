307cb6cf30b236c51e30dfec1852494a2a8c31d5
Noam <noamru22@gmail.com>
2023-12-14 18:48:07 +0100

    mip: app dir migration

--- src/app/create/listen/ListenComponent.tsx
+++ src/app/create/listen/ListenComponent.tsx
@@ -61,6 +61,7 @@ const ListenComponent = () => {
     if (metadata.audioBlob) {
       createMuxUpload(metadata.audioBlob)
         .then((uploadID) => {
+          console.log("Upload ID: ", uploadID);
           setUploadID(uploadID);
           router.push("/create/describe");
         })
--- src/app/create/listen/page.tsx
+++ src/app/create/listen/page.tsx
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import React from "react";
+=======
+>>>>>>> 8214d62 (mip: app dir migration)
 import ListenComponent from "./ListenComponent";
 
 export default function Page() {
