a9ce53a477626d8e20fe058f8949515078d67605
Noam <noamru22@gmail.com>
2024-01-22 16:47:15 +0100

    merge

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
-=======
->>>>>>> 8214d62 (mip: app dir migration)
-=======
-import React from "react";
->>>>>>> 627affc (fix: fixed import errors)
 import BlogComponent from "./BlogComponent";
 
 export default function Page() {
