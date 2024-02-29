748e5a3476dc0724142b479b1fc9c78fdd6603f6
Matt Weichel <64658667+maweiche@users.noreply.github.com>
2024-02-29 18:15:54 +0100

    added the params into the skip location route

--- src/app/create/describe/DescribeComponent.tsx
+++ src/app/create/describe/DescribeComponent.tsx
@@ -3,8 +3,8 @@
 import LoadingComponent from "../../../components/LoadingComponent";
 import { LayoutComponent } from "../../../components/layout/LayoutComponent";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useCallback } from "react";
 import useMetadataStore from "../../../utils/useMetadataStore";
 import React from "react";
 
@@ -15,11 +15,36 @@ const containerAnimation = {
 
 const DescribeComponent = () => {
   const router = useRouter();
+  const searchParams = useSearchParams()!;
   const {
-    metadata: { theVibe },
+    metadata: { theVibe, timeStamp, uploadID },
     setTheVibe,
   } = useMetadataStore();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const createQueryString = useCallback(
+    (queryParams: Record<string, string>) => {
+      const params = new URLSearchParams(searchParams);
+
+      for (const [name, value] of Object.entries(queryParams)) {
+        params.set(name, value);
+      }
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const handleChangeRoute = (route: string) => {
+    const queryParams = {
+      theVibe: theVibe,
+      uploadID: uploadID,
+      timeStamp: timeStamp,
+    };
+    const queryString = createQueryString(queryParams);
+
+    router.push(route + "?" + queryString);
+  };
 
   const handleClick = () => {
     setIsLoading(true);
@@ -29,9 +54,9 @@ const DescribeComponent = () => {
     }
 
     const locationDisabled = localStorage.getItem("locationDisabled");
-    
+
     if (locationDisabled === "true") {
-      router.push("/create/mint")
+      handleChangeRoute("/create/mint");
     } else {
       router.push("/create/locate");
     }
